28c52c9dc6a81e6c096f35ed0244662ab697a224
drea1020 <acgolindano@gmail.com>
2025-06-21 23:19:00 -0400

    Remove

--- beta/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<section id="collab">
-	<h2>Want to Collaborate? Email Me!</h2>
-    <p><a href="mailto:hello@andreagolindano.com">hello@andreagolindano.com</a></p>
-    <p class="social"><a href="http://facebook.com/andrea.golindano" target="_blank" title="Follow me on Facebook!"><i class="fa fa-facebook"></i></a> <a href="https://instagram.com/drea1020/" target="_blank" title="Follow me on Instagram!"><i class="fa fa-instagram"></i></a> <a href="https://www.linkedin.com/in/andreagolindano" target="_blank"><i class="fa fa-linkedin"></i></a></p>
-</section>
-
-<footer>
-	<p>Copyright © 2015 Andrea Golindano. All rights reserved.</p>
-</footer>
